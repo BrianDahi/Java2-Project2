e532970e97234d3262590c09aa20638457589216
BrianDahi <brian_dahi@yahoo.com>
2019-10-04 15:47:58 -0500

    more work done on MesoInherit

--- Driver.java
+++ Driver.java
@@ -45,7 +45,7 @@ public class Driver
 		System.out.println(posAvg);
 		System.out.print("\n");
 		
-		/*System.out.println("Ascii Ceiling is " + mesoInherit.calAverage()[0]);
+		System.out.println("Ascii Ceiling is " + mesoInherit.calAverage()[0]);
 		System.out.println("Ascii Floor is " + mesoInherit.calAverage()[1]);
 		System.out.println("Ascii Average is " + mesoInherit.calAverage()[2]);
 		System.out.print("\n");
@@ -53,9 +53,9 @@ public class Driver
 		System.out.println("Letter Avg: " + mesoInherit.letterAverage());
 		System.out.print("\n");
 		
-		LetterAvg letterAvg = new LetterAvg(mesoInherit.letterAverage());
-		System.out.println("Total number of stations starting with letter '" + mesoInherit.letterAverage() + "' is " + letterAvg.numberOfStationWithLetterAvg() + ".");
+		//LetterAvg letterAvg = new LetterAvg(mesoInherit.letterAverage());
+		//System.out.println("Total number of stations starting with letter '" + mesoInherit.letterAverage() + "' is " + letterAvg.numberOfStationWithLetterAvg() + ".");
 		
-		System.out.print(letterAvg);*/
+		//System.out.print(letterAvg);*/
 	}
 }
--- MesoInherit.java
+++ MesoInherit.java
@@ -12,9 +12,34 @@ public class MesoInherit extends MesoAbstract{
 	}
 	
 	int[] calAverage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
+		char a = str.charAt(0);
+		char b = str.charAt(1);
+		char c = str.charAt(2);
+		char d = str.charAt(3);
+		
+		/*int numa = (int)a;
+		int numb = (int)b;
+		int numc = (int)c;
+		int numd = (int)d;*/
+		double num = (a + b + c + d)/4.0;
+		double ceiling = Math.ceil(num);
+		double floor = Math.floor(num);
+		double avg = Math.round(num);
+		
+		int ceilingInt = (int)ceiling;
+		int floorInt = (int)floor;
+		int avgInt = (int)avg;
+		
+		int[] intArray = new int[2] ;
+		
+		
+		
+		return new int [1];
+		
+		
+		
+}
 
 	@Override
 	char letterAverage() {
